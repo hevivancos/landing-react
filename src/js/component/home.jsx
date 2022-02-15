59a898f6b8d00eb7d7130156c47b11e58f6d811d
hevivancos <vivancos.ramon@gmail.com>
2022-02-15 10:11:17 +0000

    casi hecho

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import Navbar from "./navbar.jsx"; /// ojo esta en gris porque no ha conectado ojo en mayuscula siempre
+import Jumbotron from "./jumbotron.jsx"; /// ojo esta en gris porque no ha conectado ojo en mayuscula siempre
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//estructura básica de construcción para impotar locomponentes
+
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Navbar />
+			<div className="container-fluid">
+				<Jumbotron />
+				<div className="card-group">
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</div>
+
+			<Footer />
 		</div>
 	);
 };
